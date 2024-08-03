89f95f54d07d9e87d35463124d350f73d3ed41cb
RutamBhagat <rutambhagat@gmail.com>
2024-08-04 05:14:52 +0530

    add transaction link

---
+++ pages/index.tsx
@@ -0,0 +1,56 @@
+import { Box, Button, Spacer, Stack, Text, VStack } from "@chakra-ui/react";
+
+import { AppBar } from "../components/AppBar";
+import Head from "next/head";
+import { Increment } from "../components/Increment";
+import { Initialize } from "../components/Initialize";
+import { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+
+const Home: NextPage = (props) => {
+  const [counter, setCounter] = useState("");
+  const [transactionUrl, setTransactionUrl] = useState("");
+  const wallet = useWallet();
+
+  return (
+    <div className={styles.App}>
+      <Head>
+        <title>Anchor Frontend Example</title>
+      </Head>
+      <Box h="calc(100vh)" w="full">
+        <Stack w="full" h="calc(100vh)" justify="center">
+          <AppBar />
+          <div className={styles.AppBody}>
+            {wallet.connected ? (
+              counter ? (
+                <VStack>
+                  <Increment
+                    counter={counter}
+                    setTransactionUrl={setTransactionUrl}
+                  />
+                </VStack>
+              ) : (
+                <Initialize
+                  setCounter={setCounter}
+                  setTransactionUrl={setTransactionUrl}
+                />
+              )
+            ) : (
+              <Text color="white">Connect Wallet</Text>
+            )}
+            <Spacer />
+            {transactionUrl && (
+              <a href={transactionUrl}>
+                <Button margin={8}>View most recent transaction</Button>
+              </a>
+            )}
+          </div>
+        </Stack>
+      </Box>
+    </div>
+  );
+};
+
+export default Home;
